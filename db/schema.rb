4b0c2e69b43448885a4356e72ff18ebcccf6cc36
Mantero <anh.tran@employmenthero.com>
2021-04-07 21:17:10 -0500

    Add model province

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_08_020136) do
+ActiveRecord::Schema.define(version: 2021_04_08_021506) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -38,6 +38,15 @@ ActiveRecord::Schema.define(version: 2021_04_08_020136) do
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
   end
 
+  create_table "provinces", force: :cascade do |t|
+    t.string "name"
+    t.decimal "hst"
+    t.decimal "gst"
+    t.decimal "pst"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -51,8 +60,11 @@ ActiveRecord::Schema.define(version: 2021_04_08_020136) do
     t.string "postal_code"
     t.string "address"
     t.string "city"
+    t.integer "province_id"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["province_id"], name: "index_users_on_province_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "users", "provinces"
 end
