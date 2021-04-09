cc0c65ada6e53c4ac8af7bfaab34a0e7bafe228d
Mantero <anh.tran@employmenthero.com>
2021-04-08 23:58:34 -0500

    Fix column name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_09_035751) do
+ActiveRecord::Schema.define(version: 2021_04_09_045658) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -76,13 +76,13 @@ ActiveRecord::Schema.define(version: 2021_04_09_035751) do
   create_table "order_details", force: :cascade do |t|
     t.decimal "price"
     t.decimal "quantity"
-    t.integer "products_id"
+    t.integer "product_id"
     t.integer "orders_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.decimal "tax_rate"
     t.index ["orders_id"], name: "index_order_details_on_orders_id"
-    t.index ["products_id"], name: "index_order_details_on_products_id"
+    t.index ["product_id"], name: "index_order_details_on_product_id"
   end
 
   create_table "orders", force: :cascade do |t|
@@ -139,7 +139,7 @@ ActiveRecord::Schema.define(version: 2021_04_09_035751) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "order_details", "orders", column: "orders_id"
-  add_foreign_key "order_details", "products", column: "products_id"
+  add_foreign_key "order_details", "products"
   add_foreign_key "orders", "users"
   add_foreign_key "products", "categories"
   add_foreign_key "users", "provinces"
